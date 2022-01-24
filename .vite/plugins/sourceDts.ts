d2a22f99a8b99785092a0fc705425989a2ce3da8
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2022-01-24 13:43:47 -0600

    move custom vite plugins to new .vite dir

---
+++ .vite/plugins/sourceDts.ts
@@ -0,0 +1,86 @@
+import fs from 'fs';
+import path from 'path';
+import { Plugin } from 'vite';
+import pkg from '../../package.json';
+
+const fsPromises = fs.promises;
+
+interface SourceDtsOptions {
+  mode: string;
+}
+
+/**
+ * instead of using TypeScript to generate a .d.ts file,
+ * this plugin will generate a dummy .d.ts file that points to the source code.
+ *
+ * This plugin was adapted from `vite-dts`. https://github.com/alloc/vite-dts
+ */
+export default function sourceDts(options?: Partial<SourceDtsOptions>): Plugin {
+  let dtsModule: string | undefined;
+  let typePath: string | undefined;
+  let outputFileNames: string[] = [];
+
+  return {
+    name: 'source-dts',
+    apply: (_, env) =>
+      env.command === 'build' &&
+      (options?.mode ? env.mode === options.mode : env.mode !== 'production'),
+    async configResolved(config) {
+      const { logger } = config;
+      const { outDir } = config.build;
+      const { entry, formats, fileName } = config.build.lib || {};
+
+      outputFileNames = formats.map((format) => `${fileName}.${format}.js`);
+
+      if (!entry) {
+        return logger.warn(
+          `Expected "build.lib.entry" to exist in vite config.`,
+        );
+      }
+
+      if (!pkg.types) {
+        return logger.warn(`Expected "types" to exist in package.json`);
+      }
+
+      const entryPath = path.resolve(config.root, entry);
+      const posixEntryImportPath = path
+        .relative(
+          path.resolve(config.root, path.dirname(pkg.types)),
+          entryPath.replace(/\.tsx?$/, ''),
+        )
+        .split(path.sep)
+        .join(path.posix.sep);
+
+      const entryFile = await fsPromises.readFile(entryPath, 'utf-8');
+      const hasDefaultExport =
+        /^(export default |export \{[^}]+? as default\s*[,}])/m.test(entryFile);
+
+      const baseDtsModule = `export * from "${posixEntryImportPath}"`;
+      dtsModule = hasDefaultExport
+        ? baseDtsModule.concat(
+            `\nexport {default} from "${posixEntryImportPath}"`,
+          )
+        : baseDtsModule;
+
+      typePath = path.relative(outDir, pkg.types);
+    },
+    // TODO look into replacing this with buildStart
+    generateBundle(_, bundle) {
+      const filename = Object.keys(bundle)[0];
+
+      // This make sure this file is only emitted when the first JS
+      // bundle is created and not for each bundle. This prevents the
+      // types from being emitted multiple times.
+      const isFirstBundle =
+        outputFileNames.length && outputFileNames[0] === filename;
+
+      if (isFirstBundle && typePath && dtsModule) {
+        this.emitFile({
+          type: 'asset',
+          fileName: typePath,
+          source: dtsModule,
+        });
+      }
+    },
+  };
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import fs from 'fs';
 import path from 'path';
-import { defineConfig, Plugin } from 'vite';
-import pkg from './package.json';
+import { defineConfig } from 'vite';
+import emitDts from './.vite/plugins/emitDts';
+import sourceDts from './.vite/plugins/sourceDts';
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     build: {
       lib: {
@@ -21,81 +21,6 @@ export default defineConfig(({ mode }) => {
       minify: false,
       sourcemap: true,
     },
-    plugins: [mode === 'development' && sourceDts()],
+    plugins: [sourceDts(), emitDts({ outDir: 'dist/types' })],
   };
 });
-
-const fsPromises = fs.promises;
-
-/**
- * instead of using TypeScript to generate a .d.ts file,
- * this plugin will generate a dummy .d.ts file that points to the source code.
- *
- * This plugin was adapted from `vite-dts`. https://github.com/alloc/vite-dts
- */
-function sourceDts(): Plugin {
-  let dtsModule: string | undefined;
-  let typePath: string | undefined;
-  let outputFileNames: string[] = [];
-
-  return {
-    name: 'source-dts',
-    apply: 'build',
-    async configResolved(config) {
-      const { logger } = config;
-      const { outDir } = config.build;
-      const { entry, formats, fileName } = config.build.lib || {};
-
-      outputFileNames = formats.map((format) => `${fileName}.${format}.js`);
-
-      if (!entry) {
-        return logger.warn(
-          `Expected "build.lib.entry" to exist in vite config.`,
-        );
-      }
-
-      if (!pkg.types) {
-        return logger.warn(`Expected "types" to exist in package.json`);
-      }
-
-      const entryPath = path.resolve(config.root, entry);
-      const posixEntryImportPath = path
-        .relative(
-          path.resolve(config.root, path.dirname(pkg.types)),
-          entryPath.replace(/\.tsx?$/, ''),
-        )
-        .split(path.sep)
-        .join(path.posix.sep);
-
-      const entryFile = await fsPromises.readFile(entryPath, 'utf-8');
-      const hasDefaultExport =
-        /^(export default |export \{[^}]+? as default\s*[,}])/m.test(entryFile);
-
-      const baseDtsModule = `export * from "${posixEntryImportPath}"`;
-      dtsModule = hasDefaultExport
-        ? baseDtsModule.concat(
-            `\nexport {default} from "${posixEntryImportPath}"`,
-          )
-        : baseDtsModule;
-
-      typePath = path.relative(outDir, pkg.types);
-    },
-    generateBundle(_, bundle) {
-      const filename = Object.keys(bundle)[0];
-
-      // This make sure this file is only emitted when the first JS
-      // bundle is created and not for each bundle. This prevents the
-      // types from being emitted multiple times.
-      const isFirstBundle =
-        outputFileNames.length && outputFileNames[0] === filename;
-
-      if (isFirstBundle && typePath && dtsModule) {
-        this.emitFile({
-          type: 'asset',
-          fileName: typePath,
-          source: dtsModule,
-        });
-      }
-    },
-  };
-}
