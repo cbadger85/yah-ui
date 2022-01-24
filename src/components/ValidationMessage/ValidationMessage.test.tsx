73a03fe27b54816b23796494b6a014ae98755314
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2022-01-23 21:18:40 -0600

    finish validation message tests

--- src/components/ValidationMessage/ValidationMessage.test.tsx
+++ src/components/ValidationMessage/ValidationMessage.test.tsx
@@ -4,33 +4,51 @@ import { ValidationMessage } from '.';
 
 describe('<Validation Message />', () => {
   it('should set the id to a generated value if no id is provided', () => {
-    render(<ValidationMessage />);
+    const testId = "testid";
+    const {getByTestId} = render(<ValidationMessage data-testid={testId} />);
 
-    const messageId = document.querySelector('span')?.id
-    
+    const messageId = getByTestId(testId).id;
+
     expect(messageId).toEqual(expect.any(String));
     expect(messageId).not.toHaveLength(0);
   });
 
   it('should use the id attribute to the provided id', () => {
-    const id = "message";
+    const testId = 'testid';
+    const id = 'message';
 
-    render(<ValidationMessage id={id} />);
+    const { getByTestId } = render(
+      <ValidationMessage id={id} data-testid={testId} />,
+    );
 
-    const messageId = document.querySelector('span')?.id
-    
+    const messageId = getByTestId(testId).id;
+
     expect(messageId).toBe(id);
   });
 
-    it('should set the aria-live to `polite` by default', () => {
-      render(<ValidationMessage />);
+  it('should set the aria-live to `polite` by default', () => {
+    const testId = 'testid';
+    const { getByTestId } = render(<ValidationMessage data-testid={testId} />);
 
-      const messageId = document.querySelector('span')?.id;
 
-      expect(document.querySelector('span')).toHaveAttribute('aria-live', 'polite')
-      expect(messageId).not.toHaveLength(0);
+    expect(getByTestId(testId)).toHaveAttribute('aria-live', 'polite');
+  });
+
+    it('should be a span element by default', () => {
+      const testId = 'testid';
+      const { getByTestId } = render(
+        <ValidationMessage data-testid={testId} />,
+      );
+
+      expect(getByTestId(testId)).toBeInstanceOf(HTMLSpanElement);
     });
-  
+
+        it('should become the provided element type', () => {
+          const testId = 'testid';
+          const { getByTestId } = render(
+            <ValidationMessage data-testid={testId} as="div" />,
+          );
+
+          expect(getByTestId(testId)).toBeInstanceOf(HTMLDivElement);
+        });
 });
-
-
